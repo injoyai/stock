e199133a44c1511a5c7bf7da504d7eceb5474ef2
injoyai <1113655791@qq.com>
2024-10-30 09:16:23 +0800

    整理模块

--- common/common.go
+++ common/common.go
@@ -1,6 +1,7 @@
 package common
 
 import (
+	"github.com/injoyai/base/maps"
 	"github.com/injoyai/conv/cfg/v2"
 	influx "github.com/injoyai/goutil/database/influxdb"
 	"github.com/injoyai/goutil/database/mysql"
@@ -9,9 +10,10 @@ import (
 )
 
 var (
-	DB   *xorms.Engine  //mysql,用于存储一些基本信息
-	TSDB *influx.Client //influxdb,用于存储历史数据
-	Corn = task.New()   //定时任务,用于定时请求数据等
+	DB   *xorms.Engine    //mysql,用于存储一些基本信息
+	TSDB *influx.Client   //influxdb,用于存储历史数据
+	Corn = task.New()     //定时任务,用于定时请求数据等
+	Real = maps.NewSafe() //实时数据,实时策略加载到缓存
 )
 
 func Init() error {
